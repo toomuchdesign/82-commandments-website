661f6d178500fe8c203cc0fcb7d2dcecfefe1d6c
Andrea Carraro <me@andreacarraro.it>
2020-06-02 17:29:12 +0200

    Fix sentence picker logic

--- src/server/getCommandment.js
+++ src/server/getCommandment.js
@@ -2,7 +2,8 @@ const commandments = require('82-commandments');
 
 function getCommandment() {
   // @TODO Improve sentence picker
-  const number = Math.round(Date.now() / 86400) % 82;
+  // Pick one sentce per day
+  const number = Math.round(Date.now() / 1000 / 86400) % 82;
   return {
     sentence: commandments[number],
     number,
