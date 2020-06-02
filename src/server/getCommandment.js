8f67461f1c078daee6d161af58f6039cd7c0943b
Andrea Carraro <me@andreacarraro.it>
2020-06-02 17:26:16 +0200

    Add sentence number

--- src/server/getCommandment.js
+++ src/server/getCommandment.js
@@ -3,7 +3,10 @@ const commandments = require('82-commandments');
 function getCommandment() {
   // @TODO Improve sentence picker
   const number = Math.round(Date.now() / 86400) % 82;
-  return commandments[number];
+  return {
+    sentence: commandments[number],
+    number,
+  };
 }
 
 module.exports.getCommandment = getCommandment;
--- src/server/renderPage.js
+++ src/server/renderPage.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const readFile = util.promisify(fs.readFile);
 const hogan = require('hogan.js');
 
-async function renderPage(body = '') {
+async function renderPage({ sentence, number }) {
   const template = await readFile(
     path.join(__dirname, '../client/public/index.html.mustache'),
     'utf8'
   );
 
-  return hogan.compile(template).render({ body });
+  return hogan.compile(template).render({ body: sentence, number });
 }
 
 module.exports.renderPage = renderPage;
